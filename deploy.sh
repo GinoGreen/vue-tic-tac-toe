8db362909ec7bddacce33d4d8f1458838e44a930
GinoGreen <luigiverde97@gmail.com>
2022-02-17 20:47:19 +0100

    fixes env

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ginogreen/vue-tic-tac-toe.git master:gh-pages
+git push -f git@github.com:ginogreen/vue-tic-tac-toe.git main:gh-pages
 
 cd -
